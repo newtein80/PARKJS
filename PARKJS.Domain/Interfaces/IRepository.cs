c48511862d041b03f14bf241339abb389b7708d1
nile <newtein80@gmail.com>
2019-01-23 17:23:36 +0900

    2019.01.23 Interface, Generic

--- PARKJS.Domain/Interfaces/IRepository.cs
+++ PARKJS.Domain/Interfaces/IRepository.cs
@@ -5,13 +5,29 @@ using System.Text;
 
 namespace PARKJS.Domain.Interfaces
 {
-    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    //https://slaner.tistory.com/122
+    //https://slaner.tistory.com/126
+    //https://godffs.tistory.com/209
+    //
+    public interface IRepository<T> : IDisposable where T : class
     {
-        void Add(TEntity obj);
-        TEntity GetById(Guid id);
-        IQueryable<TEntity> GetAll();
-        void Update(TEntity obj);
+        void Add(T obj);
+        T GetById(Guid id);
+        IQueryable<T> GetAll();
+        void Update(T obj);
         void Remove(Guid id);
         int SaveChanges();
     }
 }
+
+/*
+ public interface IRepository<TEntity> : IDisposable where TEntity : class
+{
+    void Add(TEntity obj);
+    TEntity GetById(Guid id);
+    IQueryable<TEntity> GetAll();
+    void Update(TEntity obj);
+    void Remove(Guid id);
+    int SaveChanges();
+}
+ */
