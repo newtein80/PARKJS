1db97e60cc9a74e6763374f6183e6d6e1e8711ef
nile <newtein80@gmail.com>
2019-01-24 17:32:50 +0900

    2019.01.24.https://github.com/jbogard/MediatR/wiki/Migration-Guide-4.x-to-5.0

---
+++ PARKJS.Domain/CommandHandlers/CommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using PARKJS.Domain.Core.Bus;
+using PARKJS.Domain.Core.Commands;
+using PARKJS.Domain.Core.Notifications;
+using PARKJS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PARKJS.Domain.CommandHandlers
+{
+    public class CommandHandler
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMediatorHandler mediatorHandler;//Command = Send, Event = Raise
+        private readonly DomainNotificationHandler domainNotificationHandler;
+
+        public CommandHandler(IUnitOfWork unitOfWork, IMediatorHandler mediatorHandler, INotificationHandler<DomainNotification> domainNotificationHandler)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mediatorHandler = mediatorHandler;
+            this.domainNotificationHandler = (DomainNotificationHandler)domainNotificationHandler;
+        }
+
+        protected void NotifyValidationErrors(Command message)
+        {
+            foreach (var error in message.ValidationResult.Errors)
+            {
+                mediatorHandler.RaiseEvent(new DomainNotification(message.MessageType, error.ErrorMessage));
+            }
+        }
+
+        public bool Commit()
+        {
+            if (domainNotificationHandler.HasNotifications())
+            {
+                return false;
+            }
+
+            if (unitOfWork.Commit())
+            {
+                return true;
+            }
+
+            mediatorHandler.RaiseEvent(new DomainNotification("Commit", "We had a problem during saving your data."));
+            return false;
+        }
+    }
+}
---
+++ PARKJS.Domain/CommandHandlers/TodoCommandHandler.cs
@@ -0,0 +1,81 @@
+﻿using MediatR;
+using PARKJS.Domain.Commands;
+using PARKJS.Domain.Core.Bus;
+using PARKJS.Domain.Core.Notifications;
+using PARKJS.Domain.Events;
+using PARKJS.Domain.Interfaces;
+using PARKJS.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//https://github.com/jbogard/MediatR/wiki/Migration-Guide-4.x-to-5.0
+namespace PARKJS.Domain.CommandHandlers
+{
+    public class TodoCommandHandler : CommandHandler,
+        IRequestHandler<RegistNewTodoCommand>,
+        IRequestHandler<RemoveTodoCommand>,
+        IRequestHandler<UpdateTodoCommand>
+    {
+        private readonly ITodoRepository todoRepository;
+        private readonly IMediatorHandler mediatorHandler;
+
+        public TodoCommandHandler(
+            ITodoRepository todoRepository,
+            IUnitOfWork unitOfWork,
+            IMediatorHandler mediatorHandler,
+            INotificationHandler<DomainNotification> domainNotificationHandler) : base(unitOfWork, mediatorHandler, domainNotificationHandler)
+        {
+            this.todoRepository = todoRepository;
+            this.mediatorHandler = mediatorHandler;
+        }
+
+        //https://github.com/jbogard/MediatR/wiki/Migration-Guide-4.x-to-5.0
+        public Task<Unit> Handle(RegistNewTodoCommand iRegistNewTodoCommand, CancellationToken cancellationToken)
+        {
+            //throw new NotImplementedException();
+            if(!iRegistNewTodoCommand.IsValid())
+            {
+                NotifyValidationErrors(iRegistNewTodoCommand);
+                //return Task.CompletedTask;
+                return Unit.Task;
+            }
+
+            var todo = new Todo(
+                Guid.NewGuid(),
+                iRegistNewTodoCommand.Seq,
+                iRegistNewTodoCommand.Description,
+                iRegistNewTodoCommand.Title,
+                iRegistNewTodoCommand.UpdateDate);
+
+            if(todoRepository.GetbySeq(todo.Seq) != null)
+            {
+                mediatorHandler.RaiseEvent(new DomainNotification(iRegistNewTodoCommand.MessageType, "The Todo has already been taken"));
+                //return Task.CompletedTask;
+                return Unit.Task;
+            }
+
+            todoRepository.Add(todo);
+
+            if (Commit())
+            {
+                mediatorHandler.RaiseEvent(new TodoRegisteredEvent(todo.Id, todo.Seq, todo.Description, todo.Title, todo.UpdateDate));
+            }
+
+            //return Task.CompletedTask;
+            return Unit.Task;
+        }
+
+        public Task<Unit> Handle(RemoveTodoCommand iRemoveTodoCommand, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Unit> Handle(UpdateTodoCommand iUpdateTodoCommand, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
